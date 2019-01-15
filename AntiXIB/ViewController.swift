adebd1bf78afa8b9d443d36e0cef1477e29a65af
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-15 17:07:59 +0300

    Initial Commit

---
+++ AntiXIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AntiXIB
+//
+//  Created by Evgeniy Ryshkov on 15/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

238329d3e88cf7ab47383d098ee901cc0103d9a8
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-15 19:07:23 +0300

    prepare is implemented

--- AntiXIB/ViewController.swift
+++ AntiXIB/ViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var topVC: ElementViewController!
+    var centerVC: ElementViewController!
+    
+    @objc func sendToTopVC() {
+        topVC.customLabel.text = "Top is here"
+    }
+    
+    @objc func sendToCenterVC() {
+        centerVC.customLabel.text = "Center VC here"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let topButton = UIBarButtonItem(title: "Send to top", style: .plain, target: self, action: #selector(sendToTopVC))
+        let centerVC = UIBarButtonItem(title: "Send to center", style: .plain, target: self, action: #selector(sendToCenterVC))
+        
+        navigationItem.rightBarButtonItems = [topButton, centerVC]
         
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "CenterSegue" {
+            centerVC = segue.destination as? ElementViewController
+        }
+        
+        if segue.identifier == "TopSegue" {
+            topVC = segue.destination as? ElementViewController
+        }
+    }
 
 }
 
